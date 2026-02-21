a5a5247847a39e00a1275b0351e4997e7fce842a
KoNyein <nyeinchanlin75@gmail.com>
2026-02-21 15:00:15 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -1,3 +1,18 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQLDATABASE', ''),
+        'USER': os.environ.get('MYSQLUSER', ''),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''),
+        'HOST': os.environ.get('MYSQLHOST', ''),
+        'PORT': os.environ.get('MYSQLPORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+    }
+}
 
 
 
