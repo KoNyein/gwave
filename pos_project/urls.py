68eccee7aa71996d4a0ef9b5c7995e47efc570cf
KoNyein <nyeinchanlin75@gmail.com>
2026-02-21 19:10:24 +0700

    Update urls.py

--- pos_project/urls.py
+++ pos_project/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     path('', include('shop.urls')),
+    
+    path('accounts/', include('allauth.urls')),
+
 ]
