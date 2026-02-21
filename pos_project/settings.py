9871a91ecfa4d4cc84a7531c87c2214d2e1d31ce
KoNyein <nyeinchanlin75@gmail.com>
2026-02-21 19:05:54 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -28,6 +28,13 @@ INSTALLED_APPS = [
     'shop',
 ]
 
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
