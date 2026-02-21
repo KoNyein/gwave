757e76b22c1e0adb8f85c4bbf693cb82214011b3
KoNyein <nyeinchanlin75@gmail.com>
2026-02-21 18:29:49 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -36,7 +36,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS': {
+        'OPTIONS': 
             'context_processors': 
                 'django.template.context_processors.debug',
                 
