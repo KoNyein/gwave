0c64d43cfc98d25a9a086befea6ef0ad1da73170
KoNyein <nyeinchanlin75@gmail.com>
2026-02-21 18:35:26 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -31,15 +31,20 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pos_project.urls'
 
+
+
 TEMPLATES = [
-    
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
-        'OPTIONS': 
-            'context_processors': 
+        'OPTIONS': {
+            'context_processors': [
                 'django.template.context_processors.debug',
-                
-                'django.template.backends.django.DjangoTemplates',
-
-            ]
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
