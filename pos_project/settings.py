304ed7bc5916a32b3a622db8e3c3aa33192e8845
KoNyein <nyeinchanlin75@gmail.com>
2026-02-21 19:04:50 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -16,6 +16,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+'allauth',
+'allauth.account',
+'allauth.socialaccount',
+'allauth.socialaccount.providers.google',
+'allauth.socialaccount.providers.facebook',
+
+    
+    
     'shop',
 ]
 
