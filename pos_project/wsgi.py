d7866885e8685052ac87ea035ad8f71cf59895f9
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 15:33:16 +0700

    Update wsgi.py

--- pos_project/wsgi.py
+++ pos_project/wsgi.py
@@ -1,5 +1,8 @@
+
+
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pos_project.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gwave.settings')
+
 application = get_wsgi_application()
-pos_project/wsgi.py
