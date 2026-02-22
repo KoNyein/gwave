526b48ab353d3b987684ba6638d0c8d4fcdb5035
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 10:14:05 +0700

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,20 +1,20 @@
 
                 
-                
                 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 import os
 
-class ShopConfig(AppConfig):
-    name = 'shop'
+class CoreConfig(AppConfig):
+    name = 'core'
 
     def ready(self):
         if os.environ.get("RAILWAY_ENVIRONMENT"):
+            from django.contrib.auth import get_user_model
             User = get_user_model()
-            if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
+
+            if not User.objects.filter(username="admin").exists():
                 User.objects.create_superuser(
-                    username=os.environ["DJANGO_SUPERUSER_USERNAME"],
-                    email=os.environ["DJANGO_SUPERUSER_EMAIL"],
-                    password=os.environ["DJANGO_SUPERUSER_PASSWORD"],
+                    "admin",
+                    "admin@gwave.site",
+                    os.environ.get("ADMIN_PASSWORD")
                 )
                 
