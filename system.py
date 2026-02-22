113e1824c7d2958cd6248c9df2febcd5f4da5ea6
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 18:07:58 +0700

    Update system.py

--- system.py
+++ system.py
@@ -1,1 +1,9 @@
-ghj
+import os
+import dj_database_url
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
