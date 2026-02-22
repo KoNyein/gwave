3803e5c290bb2b22f02cc3deb40e76e7423b4676
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 11:36:56 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me')
 
 
-from django.contrib.auth import get_user_model
+
 import os
 
 if os.environ.get("RAILWAY_ENVIRONMENT"):
