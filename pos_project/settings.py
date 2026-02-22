d3b2ab362aa893957df5350ac974e935ab236f6d
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 12:52:26 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -88,18 +88,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': os.environ.get('MYSQL_HOST'),
-        'PORT': int(os.environ.get('MYSQL_PORT') or 3306),
-    
-    }
-}
-
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
