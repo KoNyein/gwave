b77052841fb22278594c9d6c98649ca69d29c546
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 09:46:20 +0700

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,18 +1,20 @@
-from django.apps import AppConfig
+
+                
+                
+                from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 import os
 
-class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+class ShopConfig(AppConfig):
+    name = 'shop'
 
     def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-
-        if os.environ.get("DJANGO_SUPERUSER_USERNAME"):
+        if os.environ.get("RAILWAY_ENVIRONMENT"):
+            User = get_user_model()
             if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
                 User.objects.create_superuser(
                     username=os.environ["DJANGO_SUPERUSER_USERNAME"],
-                    email=os.environ.get("DJANGO_SUPERUSER_EMAIL", ""),
-                    password=os.environ["DJANGO_SUPERUSER_PASSWORD"]
+                    email=os.environ["DJANGO_SUPERUSER_EMAIL"],
+                    password=os.environ["DJANGO_SUPERUSER_PASSWORD"],
                 )
+                
