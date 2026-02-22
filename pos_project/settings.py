35f051345f5216934e5337790a152f76f95b4537
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 20:14:26 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -101,13 +101,3 @@ ACCOUNT_USERNAME_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
-import dj_database_url
-import os
-
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
