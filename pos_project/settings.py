0e94fde50a1d295925ea54dc7ab410e4c484d010
KoNyein <nyeinchanlin75@gmail.com>
2026-02-22 20:32:43 +0700

    Update settings.py

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -1,12 +1,13 @@
+import os
 import dj_database_url
-
 DATABASES = {
     "default": dj_database_url.config(
-        default=None,
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
 }
+
 
 
 
